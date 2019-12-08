44c277cf3e5c997571c3e343f77633207e20c20e
Yubeen Jo <jybin00@naver.com>
2019-12-08 09:23:20 +0900

    우선 num은 구현함.

--- finial project.py
+++ finial project.py
@@ -18,4 +18,75 @@
 # –All the articulation points and extra information to show how your
 # program can find out those articulation points.
 
-# 알아야되는 개념 : 
+# 알아야되는 개념 :
+# DFS -> 깊이 우선 탐색(은 preorder의 일반화로 볼 수 있다.) Biconnected graph -> 어느 한 노드가 끊어져도
+# 원래 그래프와 연결되면 biconnected graph라고 함. 그렇지 않은 그래프는 articulation point를 가지고 있음.
+# Num(v) -> preorder number
+# Low(v) -> 다시 돌아올 수 있는 노드의 Num(v)
+
+
+# undirected graph
+
+from queue import Queue
+
+graph = {'A': ['B', 'C'],
+         'B': ['A', 'D', 'E'],
+         'C': ['A', 'D', 'H'],
+         'D': ['B', 'C'],
+         'E': ['F', 'G'],
+         'F': ['E', 'G'],
+         'G': ['E', 'F'],
+         'H': ['C', 'I', 'J'],
+         'I': ['H', 'J'],
+         'J': ['H', 'I']}
+
+
+# def bfs(graph, start_node):
+#     visit = list()
+#     q = Queue()
+#
+#     q.put(start_node)
+#
+#     while q.qsize() > 0:
+#         node = q.get()
+#         if node not in visit:
+#             visit.append(node)
+#             for nextNode in graph[node]:
+#                 q.put(nextNode)
+#     return visit
+
+
+def dfs(graph, start_node):
+    visit = list()
+    stack = list()
+    counter = 0
+
+    stack.append(start_node)
+
+    while stack:
+        counter = counter + 1
+        node = stack.pop()
+        if node not in visit:
+            visit.extend([node, counter])
+            stack.extend(graph[node])
+
+    return visit
+
+# def AssignNum(graph, start_node):
+#     num = list()
+#     visited = list()
+#     counter = 0
+#
+#     num[node] = counter++
+#     visited[node] = Ture
+
+
+print(type(graph))
+print()
+
+for key, value in graph.items():
+    print(key, ":", value)
+print()
+print(dfs(graph, 'A'))
+for i in dfs(graph, 'A'):
+    print(i, i)
