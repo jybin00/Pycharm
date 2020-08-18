74bad6df0737d7bb24fefe59324d6f79a10d7084
Yubeen Jo <jybin00@naver.com>
2020-08-18 22:24:04 +0900

    케라스 예제 이용해서 손글씨 인식 프로그램 만들기

--- keras.py
+++
@@ -1,8 +0,0 @@
-from keras.datasets import mnist
-from keras import models
-from keras import layers
-from keras.utils import to_categorical
-
-(train_images, train_lables), (test_images, test_lables) = mnist.load_data()
-
-
