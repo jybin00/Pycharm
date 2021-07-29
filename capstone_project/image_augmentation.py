9ed55d8191a7856a3555ea81b3f8d67a5264a65d
Yubeen Jo <jybin00@naver.com>
2021-07-29 15:46:59 +0900

    파일이 너무 많아져서 GAN은 다른 폴더에 만듦.

--- capstone_project/image_augmentation.py
+++ capstone_project/image_augmentation.py
@@ -4,7 +4,7 @@ import os
 import re
 from PIL import Image, ImageOps, ImageFilter
 # edge enhance 후 그레이 스케일
-for infile in glob.glob('/Users/yubeenjo/Desktop/Capstone/오토바이번호판/정상/*.jpg'):
+for infile in glob.glob('/Users/yubeenjo/Desktop/Capstone/오토바이번호판/crop/*.jpg'):
     file, ext = os.path.splitext(infile)
     print(infile + " grey converting,,,")
     im = Image.open(infile)
@@ -15,59 +15,59 @@ for infile in glob.glob('/Users/yubeenjo/Desktop/Capstone/오토바이번호판/
     im.close()
     print(infile + " finished!")
 # 라벨링 값 생성
-for imfile in glob.glob('/Users/yubeenjo/Desktop/Capstone/오토바이번호판/정상/*.txt'):
-    file, ext = os.path.splitext(imfile)
-    open_file = open(imfile, 'r')
-    print(imfile + " open!")
-    newfile = open(file + "eng.txt", 'w')
-    while True:
-        line = open_file.readline()
-        if not line: break
-        print(line)
-        regex = re.compile('15')
-        changed_line = regex.sub('0', line, count=1)
-        print(changed_line)
-        newfile.write(changed_line)
-    newfile.close()
-    engfile = open(file+"eng.txt", 'r')
-    write_file = open(file + ".txt", 'w')
-    while True:
-        line = engfile.readline()
-        if not line: break
-        write_file.write(line)
-    open_file.close()
-    write_file.close()
-
-    print(imfile + " done!")
+# for imfile in glob.glob('/Users/yubeenjo/Desktop/Capstone/오토바이번호판/정상/*.txt'):
+#     file, ext = os.path.splitext(imfile)
+#     open_file = open(imfile, 'r')
+#     print(imfile + " open!")
+#     newfile = open(file + "eng.txt", 'w')
+#     while True:
+#         line = open_file.readline()
+#         if not line: break
+#         print(line)
+#         regex = re.compile('15')
+#         changed_line = regex.sub('0', line, count=1)
+#         print(changed_line)
+#         newfile.write(changed_line)
+#     newfile.close()
+#     engfile = open(file+"eng.txt", 'r')
+#     write_file = open(file + ".txt", 'w')
+#     while True:
+#         line = engfile.readline()
+#         if not line: break
+#         write_file.write(line)
+#     open_file.close()
+#     write_file.close()
+#
+#     print(imfile + " done!")
 
 print("그레이 스케일 증강 완료!")
 
 # 좌우 반전
-for infile in glob.glob('/Users/yubeenjo/Desktop/Capstone/오토바이번호판/정상/*.jpg'):
-    file, ext = os.path.splitext(infile)
-    print(infile + " flip converting...")
-    im = Image.open(infile)
-    im = im.convert("RGB")
-    new_im = ImageOps.mirror(im)
-    new_im.save(file+"f.jpg")
-    im.close()
-    print(infile + " finished!")
-
-for imfile in glob.glob('/Users/yubeenjo/Desktop/Capstone/오토바이번호판/정상/*.txt'):
-    file, ext = os.path.splitext(imfile)
-    open_file = open(imfile, 'r')
-    newfile = open(file + "f.txt", 'w')
-    print(imfile + " open!")
-    while True:
-        line = open_file.readline()
-        if not line: break
-        new_line = (line[0:2] + str(round(1 - float(line[2:10]), 6)) + line[10:])
-        newfile.write(new_line)
-    print(file + " done!")
-    newfile.close()
-    open_file.close()
-
-print("좌우 반전 이미지 증강 완료!")
+# for infile in glob.glob('/Users/yubeenjo/Desktop/Capstone/오토바이번호판/정상/*.jpg'):
+#     file, ext = os.path.splitext(infile)
+#     print(infile + " flip converting...")
+#     im = Image.open(infile)
+#     im = im.convert("RGB")
+#     new_im = ImageOps.mirror(im)
+#     new_im.save(file+"f.jpg")
+#     im.close()
+#     print(infile + " finished!")
+#
+# for imfile in glob.glob('/Users/yubeenjo/Desktop/Capstone/오토바이번호판/정상/*.txt'):
+#     file, ext = os.path.splitext(imfile)
+#     open_file = open(imfile, 'r')
+#     newfile = open(file + "f.txt", 'w')
+#     print(imfile + " open!")
+#     while True:
+#         line = open_file.readline()
+#         if not line: break
+#         new_line = (line[0:2] + str(round(1 - float(line[2:10]), 6)) + line[10:])
+#         newfile.write(new_line)
+#     print(file + " done!")
+#     newfile.close()
+#     open_file.close()
+#
+# print("좌우 반전 이미지 증강 완료!")
 
 print("all done")
 
