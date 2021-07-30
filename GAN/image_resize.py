580d41fe2bc17794cd9e3c8ca90cdeef8bc79c81
Yubeen Jo <jybin00@naver.com>
2021-07-30 09:07:19 +0900

    이미지 리사이즈 프로그램 만듦.

--- GAN/image_resize.py
+++ GAN/image_resize.py
@@ -2,10 +2,13 @@ import os
 import glob
 from PIL import Image
 
-files = glob.glob('./data/temp/images/*.jpg')
+files = glob.glob('/Users/yubeenjo/Desktop/Capstone/오토바이번호판/crop_fix/*.jpg')
+counter = 0
 
 for f in files:
+    counter += 1
     img = Image.open(f)
-    img_resize = img.resize((int(img.width / 2), int(img.height / 2)))
+    img_resize = img.resize((256, 150))
     title, ext = os.path.splitext(f)
-    img_resize.save(title + '_half' + ext)
+    img_resize.save('/Users/yubeenjo/Desktop/Capstone/오토바이번호판/resize/'+str(counter) + ext)
+    print(str(counter/574*100)+"% completed")
