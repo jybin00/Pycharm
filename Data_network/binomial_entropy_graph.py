45b17b5ec1846f1a5b21938e22f97840383b935a
Yubeen Jo <jybin00@naver.com>
2022-09-30 14:32:33 +0900

    주석 추가

--- Data_network/binomial_entropy_graph.py
+++ Data_network/binomial_entropy_graph.py
@@ -12,9 +12,9 @@ def b_entropy_p(n):  # 엔트로피 계산 함수
         value_hy = 0  # 엔트로피는 확률의 역수의 log2를 취한 확률변수값의 평균이기 때문에 더해줄 변수 설정
         for k in range(0, n):  # n 횟수가 변수로 설정됨.
             prob = math.factorial(n)/(math.factorial(n-k)*math.factorial(k))*pow(p, k)*pow(1-p, n-k)
-            value_hy += prob * math.log2(1/prob)
-        number_p.append(value_hy)
-    return number_p
+            value_hy += prob * math.log2(1/prob)  # 계산한 값을 다 더해야 엔트로피가 되므로 다 더해줌.
+        number_p.append(value_hy)  # 각 p에 따라 계산된 엔트로피를 리스트에 넣어줌.
+    return number_p  # 리스트를 반환.
 
 
 plt.plot(p_value, b_entropy_p(20), color='indigo', marker=',', label='n=20')
@@ -28,5 +28,3 @@ plt.legend()
 plt.xlabel('p')
 plt.ylabel('H(Y)')
 plt.show()
-
-###dfdfsfdsf
